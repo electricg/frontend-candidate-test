c19172249917314ca35936ef9a5986a27aa58ff0
electricg <electric.g@gmail.com>
2017-02-06 23:52:15 +0000

    Implemented parse function and added tests (to be removed)

--- title-test.js
+++ title-test.js
@@ -1,7 +1,7 @@
 var assert = chai.assert;
 
 describe('Title Creator', function() {
-    it('Given a query of dress, the title should be "Dress"', function() {
+    it('Given a query of "dress", the title should be "Dress"', function() {
         var state = {
             query: 'dress',
             brand: null,
@@ -79,25 +79,82 @@ describe('Title Creator', function() {
     });
 
     // Bonus tests - uncomment if you want to give them a try
-    // it('Given a query of "nordstrom+dress" and a store of "nordstrom", the title should be "Dress at Nordstrom"', function() {
-    //     var state = {
-    //         query: 'nordstrom+dress',
-    //         brand: null,
-    //         store: 'nordstrom',
-    //         category: null
-    //     };
-    //     var title = parseTitle(state);
-    //     assert.equal(title, 'Dress at Nordstrom')
-    // });
+    it('Given a query of "nordstrom+dress" and a store of "nordstrom", the title should be "Dress at Nordstrom"', function() {
+        var state = {
+            query: 'nordstrom+dress',
+            brand: null,
+            store: 'nordstrom',
+            category: null
+        };
+        var title = parseTitle(state);
+        assert.equal(title, 'Dress at Nordstrom')
+    });
 
-    // it('Given a store of "alice+olivia", the title should be "Alice+Olivia"', function() {
-    //     var state = {
-    //         query: null,
-    //         brand: null,
-    //         store: 'alice+olivia',
-    //         category: null
-    //     };
-    //     var title = parseTitle(state);
-    //     assert.equal(title, 'Alice+Olivia')
-    // });
+    it('Given a store of "alice+olivia", the title should be "Alice+Olivia"', function() {
+        var state = {
+            query: null,
+            brand: null,
+            store: 'alice+olivia',
+            category: null
+        };
+        var title = parseTitle(state);
+        assert.equal(title, 'Alice+Olivia')
+    });
+});
+
+describe('My additional tests for Title Creator', function() {
+    it('Given a store of "alexander-wang", the title should be "Alexander Wang"', function() {
+        var state = {
+            query: null,
+            brand: null,
+            store: 'alexander-wang',
+            category: null
+        };
+        var title = parseTitle(state);
+        assert.equal(title, 'Alexander Wang')
+    });
+
+    it('Given a query of "alice+olivia+dress" and a store of "alice+olivia", the title should be "Dress at Alice+Olivia"', function() {
+        var state = {
+            query: 'alice+olivia+dress',
+            brand: null,
+            store: 'alice+olivia',
+            category: null
+        };
+        var title = parseTitle(state);
+        assert.equal(title, 'Dress at Alice+Olivia')
+    });
+
+    it('Given a query of "black+alice+olivia+dress" and a store of "alice+olivia", the title should be "Black Dress at Alice+Olivia"', function() {
+        var state = {
+            query: 'black+alice+olivia+dress',
+            brand: null,
+            store: 'alice+olivia',
+            category: null
+        };
+        var title = parseTitle(state);
+        assert.equal(title, 'Black Dress at Alice+Olivia')
+    });
+
+    it('Given a query of "alexander+wang+dress" and a store of "alexander-wang", the title should be "Dress at Alexander Wang"', function() {
+        var state = {
+            query: 'alexander+wang+dress',
+            brand: null,
+            store: 'alexander-wang',
+            category: null
+        };
+        var title = parseTitle(state);
+        assert.equal(title, 'Dress at Alexander Wang')
+    });
+
+    it('Given a brand of "alice+olivia", the title should be "Alice+Olivia"', function() {
+        var state = {
+            query: null,
+            brand: 'alice+olivia',
+            store: null,
+            category: null
+        };
+        var title = parseTitle(state);
+        assert.equal(title, 'Alice+Olivia')
+    });
 });
--- title.js
+++ title.js
@@ -3,9 +3,86 @@
  * object will be passed through there. If you want to see exactly what is passed in each case, take
  * a look in `title-test.js`
  * @param {object} state - the query object representing a search
+ * @returns {string} generated title
  */
-function parseTitle (state) {
-    var title;
+function parseTitle(state) {
+    var title = '';
+
+    var qS = '+'; // query separator
+    var bS = '-'; // brand separator
+    var sS = '-'; // store separator
+    var bP = ' by '; // brand prefix
+    var sP = ' at '; // store prefix
+
+    var generatedQuery = parseSearch(state.query, qS);
+    var generatedBrand = parseSearch(state.brand, bS);
+    var generatedStore = parseSearch(state.store, sS);
+
+    // search for store vlue inside query value
+    if (state.query && state.store) {
+        // split the words inside the store value and join them with the query separator
+        var store = state.store.split(/[^\w]/).join(qS);
+        // create store string to search inside query value and escape it
+        // add the query separator either at the end or the beginning of the
+        // store value so it will be subsequently removed
+        var storeRegEx = escapeRegExp(store + qS) + '|' + escapeRegExp(qS + store);
+        storeRegEx = new RegExp(storeRegEx);
+
+        if (storeRegEx.test(state.query)) {
+            // remove store value from query value
+            generatedQuery = state.query.replace(storeRegEx, '');
+            generatedQuery = parseSearch(generatedQuery, qS);
+        }
+    }
+
+    title += generatedQuery;
+
+    if (title && generatedBrand) {
+        title += bP;
+    }
+    title += generatedBrand;
+
+    if (title && generatedStore) {
+        title += sP;
+    }
+    title += generatedStore;
 
     return title;
+}
+
+/**
+ * Escape text to use in regex
+ * @param {string} str - text to escape
+ * @returns {string} escaped text
+ */
+function escapeRegExp(str) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+}
+
+/**
+ * Convert first letter of every word to uppercase and the rest to lowercase
+ * @param {string} str - the text to convert
+ * @returns {string} converted text
+ */
+function capitalize(str) {
+    return str.replace(/\w*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+/**
+ * Parse search text
+ * @param {string} str - the text to parse
+ * @param {string} spacer - the text to replace with a blank space
+ * @returns {string} parsed text
+ */
+function parseSearch(str, spacer) {
+    var search = '';
+
+    if (str) {
+        search = str.split(spacer).join(' ');
+        search = capitalize(search);
+    }
+
+    return search;
 }
